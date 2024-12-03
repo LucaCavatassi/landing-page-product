993a0f6e9b9eb1e44ff3f1be2a1926bb4e60a8ac
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-03 16:02:07 +0100

    feat: buttons wrapped in achor tags to redirect in new page tab

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,11 +4,13 @@
 <template>
     <div class="grey-bg d-flex justify-content-center">
         <div class="ms-container body-font">
-            <h2 class="mb-0 text-center font-size-big px-4 px-md-5 py-3 fw-bold black-text">Iscriviti per rimanere aggiornato</h2>
+            <h2 class="mb-0 text-center font-size-big px-4 px-md-5 py-3 fw-bold black-text">Iscriviti per rimanere
+                aggiornato</h2>
             <p class="fs-6 text-center">Iscriviti alla nostra newsletter per rimanere aggiornato su sconti e novità</p>
-            
+
             <form class="mb-5 d-flex align-items-center" action="">
-                <input class="ps-0 w-100 form-control" type="email" placeholder="Lascia la tua email qui..." aria-label="Email Input Newsletter">
+                <input class="ps-0 w-100 form-control" type="email" placeholder="Lascia la tua email qui..."
+                    aria-label="Email Input Newsletter">
                 <button id="newsletter-btn" type="submit">
                     <!-- SVG symbol provided by Designer -->
                     <svg class="icon" width="36" height="36" fill="currentColor">
@@ -18,26 +20,31 @@
             </form>
 
             <div class="ms-socials mb-4 d-flex justify-content-center gap-3">
-                <svg class="icon" width="42" height="42" fill="currentColor">
+                <a target="_blank" href="https://github.com/LucaCavatassi/landing-page-product">
+                    <svg class="icon" width="42" height="42" fill="currentColor">
                         <use xlink:href="#facebook"></use>
-                </svg>
-
-                <svg class="icon" width="42" height="42" fill="currentColor">
+                    </svg>
+                </a>
+                <a target="_blank" href="https://github.com/LucaCavatassi/landing-page-product">
+                    <svg class="icon" width="42" height="42" fill="currentColor">
                         <use xlink:href="#twitter"></use>
-                </svg>
-
-                
-                <svg class="icon" width="42" height="42" fill="currentColor">
-                    <use xlink:href="#youtube"></use>
-                </svg>
-                
-                <svg class="icon" width="42" height="42" fill="currentColor">
-                    <use xlink:href="#linkedin"></use>
-                </svg>
-
-                <svg class="icon" width="42" height="42" fill="currentColor">
-                    <use xlink:href="#instagram"></use>
-                </svg>
+                    </svg>
+                </a>
+                <a target="_blank" href="https://github.com/LucaCavatassi/landing-page-product">
+                    <svg class="icon" width="42" height="42" fill="currentColor">
+                        <use xlink:href="#youtube"></use>
+                    </svg>
+                </a>
+                <a target="_blank" href="https://github.com/LucaCavatassi/landing-page-product">
+                    <svg class="icon" width="42" height="42" fill="currentColor">
+                        <use xlink:href="#linkedin"></use>
+                    </svg>
+                </a>
+                <a target="_blank" href="https://github.com/LucaCavatassi/landing-page-product">
+                    <svg class="icon" width="42" height="42" fill="currentColor">
+                        <use xlink:href="#instagram"></use>
+                    </svg>
+                </a>
             </div>
         </div>
     </div>
@@ -61,7 +68,7 @@
     max-width: 540px;
     width: 100%;
     padding: 0 15px;
-    box-sizing: border-box; 
+    box-sizing: border-box;
 }
 
 .font-size-big {
@@ -79,9 +86,10 @@
 form {
     position: relative;
 }
+
 .form-control {
     background-color: $grey;
-    border-bottom: 1px solid lightgray!important;
+    border-bottom: 1px solid lightgray !important;
     border-top: none;
     border-left: none;
     border-right: none;
@@ -113,12 +121,16 @@ form {
     .icon:hover {
         transform: scale(1.1);
     }
-    
+
 }
 
 // Socials
 .ms-socials {
     color: $green;
+
+    a {
+        color: inherit!important;
+    }
     .icon {
         border: 1px solid lightgrey;
         padding: 0.5rem;
@@ -129,8 +141,7 @@ form {
     .icon:hover {
         transform: scale(1.1);
         cursor: pointer;
-        box-shadow: 0 0 0.25rem 0.25rem $green;
-
+        box-shadow: 0 0.3rem 0.3rem -0.25rem $green;
     }
 }
 </style>
