697b6759f86d4124f91e440c59a51a4cc37e743a
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-03 15:46:21 +0100

    feat: button postiion and styling

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,8 +7,14 @@
             <h2 class="mb-0 text-center font-size-big px-4 px-md-5 py-3 fw-bold black-text">Iscriviti per rimanere aggiornato</h2>
             <p class="fs-6 text-center">Iscriviti alla nostra newsletter per rimanere aggiornato su sconti e novità</p>
             
-            <form class="mb-5" action="">
+            <form class="mb-5 d-flex align-items-center" action="">
                 <input class="ps-0 w-100 form-control" type="email" placeholder="Lascia la tua email qui..." aria-label="Email Input Newsletter">
+                <button id="newsletter-btn" type="submit">
+                    <!-- SVG symbol provided by Designer -->
+                    <svg class="icon" width="36" height="36" fill="currentColor">
+                        <use xlink:href="#send"></use>
+                    </svg>
+                </button>
             </form>
         </div>
     </div>
@@ -47,6 +53,9 @@
 }
 
 // FORM
+form {
+    position: relative;
+}
 .form-control {
     background-color: $grey;
     border-bottom: 1px solid grey!important;
@@ -62,5 +71,24 @@
     border-color: $green;
     outline: 0;
     box-shadow: 0 0.3rem 0.3rem -0.25rem $green;
+}
+
+// Submit Button
+#newsletter-btn {
+    position: absolute;
+    right: 0;
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+
+    svg.icon {
+        fill: $green!important;
+        transition: transform 0.2s ease-in-out;
+    }
+
+    &:hover svg.icon {
+        transform: scale(1.1); // Slight zoom on hover
+    }
 }
 </style>
