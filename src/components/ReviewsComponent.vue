593617052b560a2995398062db06fa10314118f2
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-04 16:32:12 +0100

    feat: installed vue carousel for reviews carousel

--- src/components/ReviewsComponent.vue
+++ src/components/ReviewsComponent.vue
@@ -10,11 +10,21 @@
 
 <template>
     <div id="reviews">
-        <h2>Cosa ne pensano i nostri sviluppatori</h2>
-        <cardComponent/>
+        <div class="container">
+            <h2 class="fw-bold text-center">Cosa ne pensano i nostri sviluppatori</h2>
+            <cardComponent/>
+        </div>
     </div>
 </template>
 
 <style>
+    h2 {
+        font-size: 3rem;
+    }
 
+    @media (min-width: 576px) {
+        h2 {
+            font-size: 2.5rem!important;
+        }
+    }
 </style>
