fe27157540f74782ec8e57e46b507bff9e91e314
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-04 17:08:21 +0100

    feat: slider works in autoplay

--- src/components/ReviewsComponent.vue
+++ src/components/ReviewsComponent.vue
@@ -1,29 +1,53 @@
 <script>
-    import cardComponent from './subComponents/cardComponent.vue';
-    import { Carousel, Slide } from 'vue-carousel';
-    
-    export default {
-        components: {
-            cardComponent,
-            Carousel,
-            Slide
+import cardComponent from './subComponents/cardComponent.vue';
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+
+export default {
+    components: {
+        cardComponent,
+        Carousel,
+        Slide,
+        Navigation
+    },
+    data() {
+        return {
+            reviews: [
+                {
+                    description: "La paperella di gomma è diventata il mio strumento di debugging preferito. Spiegare i problemi al mio 'assistente' silenzioso mi ha aiutato a risolvere bug che altrimenti mi avrebbero fatto impazzire. È anche un ottimo antistress nelle giornate difficili!",
+                    date: 'Feb 22, 2023',
+                    name: 'David William'
+                },
+                {
+                    description: "Non avrei mai immaginato quanto una semplice paperella di gomma potesse fare la differenza. È incredibile come verbalizzare i problemi davanti alla paperella mi aiuti a vedere soluzioni che prima non notavo. Inoltre, è carinissima sulla mia scrivania!",
+                    date: "April 5, 2023",
+                    name: "Emma Turner"
+                },
+                {
+                    description: "Da quando ho adottato la paperella di gomma, il mio processo di debugging è migliorato notevolmente. La trovo molto utile per schiarirmi le idee e trovare errori nascosti. La porto sempre con me, ovunque vada!",
+                    date: "April 18, 2023",
+                    name: "Rachel Bryan"
+                },
+                {
+                    description: "La mia paperella di gomma è il miglior collega che potessi chiedere. Sempre pronta ad ascoltare, mai giudicante. È sorprendente come qualcosa di così semplice possa essere così efficace nel risolvere problemi complessi. E non dimentichiamo che è anche adorabile!",
+                    date: "Park C",
+                    name: "May 22, 2023"
+                },
+            ]
         }
     }
+}
 </script>
 
 <template>
     <div id="reviews">
         <div class="container">
             <h2 class="fw-bold text-center">Cosa ne pensano i nostri sviluppatori</h2>
-            <Carousel>
-                <Slide>
-                    <cardComponent/>
-                </Slide>
-                <Slide>
-                    <cardComponent/>  
-                </Slide>
-                <Slide>
-                    <cardComponent/>  
+            <Carousel :autoplay="4000" wrap-around pause-autoplay-on-hover>
+                <Slide v-for="review in reviews" :key="review">
+                    <div class="carousel__item">            
+                        <cardComponent :review="review" />
+                    </div>
                 </Slide>
             </Carousel>
         </div>
@@ -31,13 +55,13 @@
 </template>
 
 <style>
-    h2 {
-        font-size: 3rem;
-    }
+h2 {
+    font-size: 3rem;
+}
 
-    @media (min-width: 576px) {
-        h2 {
-            font-size: 2.5rem!important;
-        }
+@media (min-width: 576px) {
+    h2 {
+        font-size: 2.5rem !important;
     }
+}
 </style>
