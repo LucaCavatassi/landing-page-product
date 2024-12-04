dd67acde1616cad7900de8cbbb35dec547348aa4
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-04 10:45:06 +0100

    feat: jumbo right side different approach for more clean code

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -7,13 +7,10 @@
             <div class="col-12 col-sm-6">
                 <img src="/imgs/banner-image.png" alt="duck image">
             </div>
-            <div id="cta-bg" class="col-12 col-sm-6">
-                <div>
-                    <img src="/imgs/banner-bg.png" alt="duck background">
-                </div>
-                <div id="cta-text" class="postion-absolute black-text text-center">
-                    <h2 class="m-2">Rubber Ducks Debuggings</h2>
-                    <p class="m-2">Spiegare il codice a una paperella di gomma aiuta i programmatori a chiarire il pensiero e individuare errori nascosti.</p>
+            <div class="col-12 col-sm-6">
+                <div id="cta-text" class="black-text text-center mt-2 p-4">
+                    <h2 class="mt-2 fw-bold">Rubber Ducks Debuggings</h2>
+                    <p class="mt-2">Spiegare il codice a una paperella di gomma aiuta i programmatori a chiarire il pensiero e individuare errori nascosti.</p>
                     <div class="mt-5">
                         <a href="" class="text-uppercase mt-3 p-3">Acquistala subito!</a>
                     </div>
@@ -25,26 +22,34 @@
 
 <style scoped lang="scss">
 @import '../style/general.scss';
+
+@media (min-width: 560px) {
+    .row {
+        height: 50vh;
+        overflow: hidden;
+    }
+}
+
+.col-12 {
+    height: 100%;
+    background-image: url(imgs/banner-bg.png);
+    aspect-ratio: 1;
+    background-size: cover; // Ensure the image covers the entire element
+    background-position: center; // Center the image
+    background-repeat: no-repeat; // Prevent repeating the image
+}
+
 // Img Styling
 img {
     display: block;
-    max-width: 100%;
-    height: auto;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
 }
 // CTA
-#cta-bg {
-    position: relative;
-    z-index: 0;
-}
 #cta-text {
     width: 100%;
     height: 100%;
-    position: absolute;
-    z-index: 999;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     h2 {
         font-size: 3rem;
     }
