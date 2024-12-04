3fc75ad23da884fbb44f63c5723df1a880e5c3b6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-04 09:56:11 +0100

    feat: jumbo layout

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -2,19 +2,24 @@
 </script>
 
 <template>
-    <div class="container body-font">
-        <div class="row" id="home">
-            <div class="col-12 col-md-6">
-                <img class="img-fluid" src="/imgs/banner-bg.png" alt="">
+    <div class="container-fluid"></div>
+        <div class="row gx-0" id="home">
+            <div class="col-12 col-sm-6">
+                <img src="/imgs/banner-image.png" alt="duck image">
             </div>
-            <div class="col-12 col-md-6">
-
+            <div class="col-12 col-sm-6">
+                <img src="/imgs/banner-bg.png" alt="duck background">
             </div>
         </div>
-    </div>
 </template>
 
 <style scoped lang="scss">
 @import '../style/general.scss';
 
+img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+}
 </style>
