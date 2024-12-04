8fcf6764ee400d78d2a5e8943705023c97e64485
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-04 11:07:04 +0100

    feat: text finally working right in every breakpoint

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -8,11 +8,11 @@
                 <img src="/imgs/banner-image.png" alt="duck image">
             </div>
             <div class="col-12 col-sm-6">
-                <div id="cta-text" class="black-text text-center mt-2 p-4">
-                    <h2 class="mt-2 fw-bold">Rubber Ducks Debuggings</h2>
-                    <p class="mt-2">Spiegare il codice a una paperella di gomma aiuta i programmatori a chiarire il pensiero e individuare errori nascosti.</p>
-                    <div class="mt-5">
-                        <a href="" class="text-uppercase mt-3 p-3">Acquistala subito!</a>
+                <div id="cta-text" class="black-text text-center d-flex flex-column px-3 align-items-center mt-4 px-0 px-md-5">
+                    <h2 class="fw-bold mb-0">Rubber Ducks Debuggings</h2>
+                    <p class="">Spiegare il codice a una paperella di gomma aiuta i programmatori a chiarire il pensiero e individuare errori nascosti.</p>
+                    <div class="">
+                        <a href="" class="text-uppercase mt-3 py-2 px-3">Acquistala subito!</a>
                     </div>
                 </div>
             </div>
@@ -23,15 +23,30 @@
 <style scoped lang="scss">
 @import '../style/general.scss';
 
-@media (min-width: 560px) {
+@media (min-width: 576px) {
     .row {
         height: 50vh;
         overflow: hidden;
     }
+    .col-12 {
+        height: 100%!important;
+    }
+}
+
+@media (min-width: 768px) {
+    h2 {
+        font-size: 3rem!important;
+    }
+}
+
+@media (min-width: 1400px) {
+    h2 {
+        font-size: 4rem!important;
+    }
 }
 
 .col-12 {
-    height: 100%;
+    height: calc(100vh / 2);
     background-image: url(imgs/banner-bg.png);
     aspect-ratio: 1;
     background-size: cover; // Ensure the image covers the entire element
@@ -51,7 +66,7 @@ img {
     width: 100%;
     height: 100%;
     h2 {
-        font-size: 3rem;
+        font-size: 2.5rem;
     }
     a {
         color: inherit;
