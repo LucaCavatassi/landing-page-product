6390270ac08cf9b63917846b52b3177910700e2f
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-05 10:16:14 +0100

    feat: shopcard component creation with props from parent + import on parent

--- src/components/ShopComponent.vue
+++ src/components/ShopComponent.vue
@@ -1,14 +1,34 @@
 <script>
+    import ShopCardComponent from './subComponents/shopCardComponent.vue';
+
+    export default {
+        components: {
+            ShopCardComponent
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+
 </script>
 
 <template>
     <div id="shop" class="p-4">
-        <!-- Infotext -->
-        <div class="text-center info-text">
-            <h1 class="fw-bold">La collezione di paperelle</h1>
-            <p>Una paperella per ogni stile di programmazione</p>
+        <div class="container">
+            <!-- Infotext -->
+            <div class="text-center info-text">
+                <h1 class="fw-bold">La collezione di paperelle</h1>
+                <p>Una paperella per ogni stile di programmazione</p>
+            </div>
+            <!-- /Infotext -->
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <ShopCardComponent/>
+                </div>
+            </div>
         </div>
-        <!-- /Infotext -->
         
     </div>
 </template>
---
+++ src/components/subComponents/shopCardComponent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: {
+        items: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+    @import '../style/general.scss';
+</style>
