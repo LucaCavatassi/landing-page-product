315847589b42d744c1d75e84acd2fbaad9de9204
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-12-05 11:23:07 +0100

    feat: unique item with cycle

--- src/components/subComponents/faqsAccordionComponent.vue
+++ src/components/subComponents/faqsAccordionComponent.vue
@@ -12,62 +12,26 @@ export default {
 <template>
     <div class="m-4">
         <div class="accordion" id="myAccordion">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                    <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne">{{ questions[0].question }}</button>
+            <!-- ITEM-->
+            <div class="accordion-item" v-for="(question, index) in questions" :key="index">
+                <!-- Question -->
+                <h2 class="accordion-header" :id="'heading' + index">
+                    <button type="button" class="accordion-button collapsed fw-bold" data-bs-toggle="collapse" :data-bs-target="'#collapse' + index" 
+                    aria-expanded="false" :aria-controls="'collapse' + index">
+                        {{ question.question }}
+                    </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <!-- /Question -->
+
+                <!-- Answer -->
+                <div :id="'collapse' + index" class="accordion-collapse collapse" :aria-labelledby="'heading' + index" data-bs-parent="#myAccordion">
                     <div class="card-body">
-                        <p>HTML stands for HyperText Markup Language. HTML is the standard markup language for
-                            describing the structure of web pages. <a
-                                href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a>
-                        </p>
+                        <p>{{ question.answer }}</p>
                     </div>
                 </div>
+                <!-- /Answer -->
             </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="headingTwo">
-                    <button type="button" class="accordion-button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseTwo">{{ questions[1].question }}</button>
-                </h2>
-                <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
-                    <div class="card-body">
-                        <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web
-                            development. It is a collection of CSS and HTML conventions. <a
-                                href="https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/"
-                                target="_blank">Learn more.</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="headingThree">
-                    <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
-                        data-bs-target="#collapseThree">{{ questions[2].question }}</button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
-                    <div class="card-body">
-                        <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for
-                            a given HTML element such as colors, backgrounds, fonts etc. <a
-                                href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="headingThree">
-                    <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
-                        data-bs-target="#collapseThree">{{ questions[3].question }}</button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
-                    <div class="card-body">
-                        <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for
-                            a given HTML element such as colors, backgrounds, fonts etc. <a
-                                href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <!-- /ITEM-->
         </div>
     </div>
 </template>
